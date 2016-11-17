b3cb54cc1936250a0f4dc36a0421f8490765b31f
AbdoHachem <abdo324_hachem@hotmail.com>
2016-11-17 14:52:56 +0400

    Java class part of the events package

--- Alert.java
+++ Alert.java
@@ -1,23 +1,10 @@
-package endUsers;
+package events;
 
-public class Alert {
 
-	public Alert(){
-		severity = 0;
-		message = "NoMessage";
-	}
-	
-	public boolean setAlert(int sev, String message){	//Returns true if alert was set.
-		if(sev > 5 || sev < 0)
-			return false;
-		else{
-			this.severity = sev;
-			this.message = message;
-			return true;
-		}
-	}
-	
-	
-	private int severity; 	//We will use a scale of 1-5
-	private String message;
+public class Alert extends Notice {
+	public Alert(long count, java.sql.Timestamp timestamp, long busId, long parentId,
+			String message){
+		super(count, timestamp, busId, parentId, message);
+	}	
 }
+
